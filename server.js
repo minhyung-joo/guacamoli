a8c6182bffcc89e0d45f30d84257ea506bb70f6d
hjchoi95 <hongjoonchoi95@gmail.com>
2016-04-19 18:26:02 +0800

    dsasdd

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(bodyParser.json());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+console.log("fuckkkkkk");
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
