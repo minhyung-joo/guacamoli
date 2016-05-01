3ea2123a6348a2a6fb75675d3e56952a198579ec
hjchoi95 <hongjoonchoi95@gmail.com>
2016-05-01 12:27:40 +0800

    dd

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var pg = require('pg');
 var app = express();
 
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var DATABASE_URL = "postgresql://"+process.env.OPENSHIFT_POSTGRESQL_DB_HOST+":"+process.env.OPENSHIFT_POSTGRESQL_DB_PORT;
+//var DATABASE_URL = "postgresql://"+process.env.OPENSHIFT_POSTGRESQL_DB_HOST+":"+process.env.OPENSHIFT_POSTGRESQL_DB_PORT;
+var DATABASE_URL = "postgresql://admin5bxzk4e:7t32Pgi5GR_e@127.6.45.2:5432/guacamoliii";
 
 app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 8080));
 
