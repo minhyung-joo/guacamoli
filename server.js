2113ca78f638cb73749ec01d208aca33ef9b909c
hjchoi95 <hongjoonchoi95@gmail.com>
2016-09-10 23:18:13 +0800

    admin_routes restored

--- server.js
+++ server.js
@@ -102,6 +102,52 @@ app.get('/admin_only_update_menu/:menuId', function (req, res) {
   });
 });
 
+
+app.get('/menu/:menuId', function (req, res) {
+  console.log("/menu/params menuID = " + req.params.menuId);
+  pg.connect(DATABASE_URL, function(err, client, done) {
+    client.query("SELECT * FROM meal "+
+                  "WHERE meal.id = $1",
+                  [req.params.menuId],
+                  function(err, result) {
+      if (err)
+      {
+        console.error(err); res.send("Error " + err);
+      }
+      else
+      {
+        console.log("menu select result");
+        console.log(result.rows);
+        res.render('pages/menu', {result: result.rows[0]});
+      }
+    });
+    done();
+  });
+});
+
+app.get('/admin_only_update_menu/:menuId', function (req, res) {
+  console.log("/admin_only_update_menu/params menuID = " + req.params.menuId);
+  pg.connect(DATABASE_URL, function(err, client, done) {
+    client.query("SELECT * FROM meal "+
+                  "WHERE meal.id = $1",
+                  [req.params.menuId],
+                  function(err, result) {
+      if (err)
+      {
+        console.error(err); res.send("Error " + err);
+      }
+      else
+      {
+        console.log("menu select result");
+        console.log(result.rows);
+        res.render('pages/updateMenu', {result: result.rows[0]});
+      }
+    });
+    done();
+  });
+});
+
+
 app.get('/admin_only_insert_menu', function(request, response) {
   response.render('pages/insertMenu');
 });
