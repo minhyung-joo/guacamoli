fed286a6e4d8ab7ade9675d4749f60801c8ffc16
hjchoi95 <hongjoonchoi95@gmail.com>
2016-09-25 23:22:39 +0800

    changed server config to be compatible with ITSC server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 //////////////////////////////////////////////////////////
 // configurations
 //////////////////////////////////////////////////////////
+var RUNNING_ON_ITSC_SERVER = false;
 var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
@@ -14,29 +15,46 @@ var app = express();
 
 var path = require("path");
 
-
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP; // only for openshift
 var DATABASE_URL;
 
-//////////////// on openshift
-if (process.env.OPENSHIFT_NODEJS_PORT) {
-  DATABASE_URL = process.env.OPENSHIFT_POSTGRESQL_DB_URL;//"postgresql://adminmnvdci3:72fAHfuEN5q6";
-
-  app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 8080));
-  app.listen(app.get('port'), server_ip_address, function() {
-    console.log('Node (openshift) app is running on port', app.get('port'));
+// //////////////// on openshift
+// if (process.env.OPENSHIFT_NODEJS_PORT) {
+//   DATABASE_URL = process.env.OPENSHIFT_POSTGRESQL_DB_URL;//"postgresql://adminmnvdci3:72fAHfuEN5q6";
+//
+//   app.set('port', (process.env.OPENSHIFT_NODEJS_PORT || 8080));
+//   app.listen(app.get('port'), server_ip_address, function() {
+//     console.log('Node (openshift) app is running on port', app.get('port'));
+//   });
+// }
+// /////////////// on heroku or local
+// else {
+//   DATABASE_URL = process.env.DATABASE_URL
+//     || "postgres://bibcnlyezwlkhl:gdhvCdkdw5znI-LjSspT6wKOfR@ec2-54-225-223-40.compute-1.amazonaws.com:5432/davktp8lndlj83"+'?ssl=true';
+//
+//   app.set('port', (process.env.PORT || 5000));
+//   app.listen(app.get('port'), function() {
+//     console.log('Node (heroku & local) app is running on port', app.get('port'));
+//   });
+// }
+if (RUNNING_ON_ITSC_SERVER) {
+  process.chdir("/home/guacamoli/guacamoli");
+  DATABASE_URL = "postgres://guacamoli:1234@localhost:5432/guacamoli";
+  app.set('port', 80);
+  app.listen(app.get('port'), function() {
+    console.log('Node (ust.hk) app is running on port', app.get('port'));
   });
 }
-/////////////// on heroku or local
 else {
-  DATABASE_URL = process.env.DATABASE_URL
-    || "postgres://bibcnlyezwlkhl:gdhvCdkdw5znI-LjSspT6wKOfR@ec2-54-225-223-40.compute-1.amazonaws.com:5432/davktp8lndlj83"+'?ssl=true';
+  DATABASE_URL = process.env.DATABASE_URL;
+  || "postgres://bibcnlyezwlkhl:gdhvCdkdw5znI-LjSspT6wKOfR@ec2-54-225-223-40.compute-1.amazonaws.com:5432/davktp8lndlj83"+'?ssl=true';
 
   app.set('port', (process.env.PORT || 5000));
   app.listen(app.get('port'), function() {
     console.log('Node (heroku & local) app is running on port', app.get('port'));
   });
 }
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
