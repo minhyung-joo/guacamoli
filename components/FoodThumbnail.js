d15d755199b7391988082920cfe805e333fae523
nayeon7lee <nayeon7lee@gmail.com>
2016-11-04 23:40:27 +0800

    test

--- components/FoodThumbnail.js
+++ components/FoodThumbnail.js
@@ -28,7 +28,7 @@ export default class FoodThumbnailComponent extends React.Component {
         return (
             <Card style={{marginBottom:25}}>
                 <CardMedia>
-                    <img src={imageUrlMapper(processedPath)} height="175" width="100%"/>
+                    <img src={imageUrlMapper(food.picture_url)} height="175" width="100%"/>
                 </CardMedia>
                 <CardTitle title={titleHandler(food.name)} subtitle={restaurantList[restaurantId] + " - " + food.price + " HKD"} />
             </Card>
