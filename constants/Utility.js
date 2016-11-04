f305bbf896f09b84b915523d0853a6be7884fb07
nayeon7lee <nayeon7lee@gmail.com>
2016-11-04 23:03:30 +0800

    debug

--- constants/Utility.js
+++ constants/Utility.js
@@ -57,7 +57,10 @@ export function tasteMapper(ids){
 
 export function imageUrlMapper(relativePath){
     const absoluteBasePath = 'http://guacamolistorage-hkust25.appcloud.ust.hk/uploads/imageurl';
-    //var processedPath = relativePath.replace('.jpg', '_S.jpg');
+    var processedPath = relativePath.replace('.jpg', '_S.jpg');
+    console.log(processedPath);
+    var abs = absoluteBasePath;
+    console.log(abs.replace('imageurl',processedPath));
     return absoluteBasePath.replace('imageurl', relativePath);
 }
 
