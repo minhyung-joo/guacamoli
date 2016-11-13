bee9cbbaa4b589fa34bb362e4bb4ab4dab3cac2c
nayeon7lee <nayeon7lee@gmail.com>
2016-11-13 23:13:37 +0800

    tried to make the ranking page ui to be prettier

--- components/RankingItem.js
+++ components/RankingItem.js
@@ -24,9 +24,11 @@ const styles = {
         fontSize:25, color:'black'
     },
     rankingDetailStyle:{
-        margin:0, padding:0, color:'black'
+        margin:0, padding:0, color:'black',
+        fontSize: '80%'
     },
     rankingItemStyle:{
-        marginTop:20
+        marginTop:20,
+        height:70
     }
 };
