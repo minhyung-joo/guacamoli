d996ff2ca742762ca72b3e4a5e43686789259b2f
Hong Joon <hongjoon@Hongs-MacBook-Air.local>
2016-12-07 13:47:31 +0800

    added /updateMeal and /api/auth, and also added authentication for data-sensitive routines

--- backend_APIs/admin_APIs.js
+++ backend_APIs/admin_APIs.js
@@ -32,8 +32,6 @@ var init = function(app, DATABASE_URL) {
     console.log("POST /uploadHandler");
 
     //TODO:
-    console.log("auth:");
-    console.log(auth);
     // add code here
     if (auth) {
       if (req.body.password!=auth.lg1 || req.body.password!=auth.apc ||
@@ -43,8 +41,6 @@ var init = function(app, DATABASE_URL) {
         return;
       }
     }
-
-
 
     console.log(req.file);
     if (!req.file || !req.file.originalname) {
@@ -172,44 +168,106 @@ var init = function(app, DATABASE_URL) {
   });
 
 
+  /*
+    NEW VERSION OF meal update API
+  */
+  app.post('/updateMeal', function (req, res, next) {
+    console.log("POST /updateMeal");
 
-
-
-  // DEPRECIATED
-  app.post('/uploadMeal', function (request, response) {
-    if (!request.body.name || !request.body.picture_url) {
-      console.log("upload meal failed: required fields null");
-      response.status(200).send({"status":"FAIL: required fields null"});
-      return;
+    //TODO:
+    // add code here
+    if (auth) {
+      if (req.body.password!=auth.lg1 || req.body.password!=auth.apc ||
+        req.body.password!=auth.grb || req.body.password!=auth.millano) {
+        console.log("ERROR: authentication failed");
+        res.send({status:"ERROR: authentication failed"});
+        return;
+      }
     }
-    if (!request.body.price) {
-      request.body.price = 0;
+
+    if (req.body.offeredTimes) {
+      req.body.offeredTimes = req.body.offeredTimes.split(",");
+    } else {
+      req.body.offeredTimes=[];
     }
+    if (req.body.tasteTypes) {
+      req.body.tasteTypes = req.body.tasteTypes.split(",");
+    } else {
+      req.body.tasteTypes=[];
+    }
+    if (req.body.foodTypes) {
+      req.body.foodTypes = req.body.foodTypes.split(",");
+    } else {
+      req.body.foodTypes=[];
+    }
+    if (req.body.sauceTypes) {
+      req.body.sauceTypes = req.body.sauceTypes.split(",");
+    } else {
+      req.body.sauceTypes=[];
+    }
+
     pg.connect(DATABASE_URL, function(err, client, done) {
       client.query("INSERT INTO meal"+
                     "(restaurantId, name, chineseName, category, price, picture_url, "+
                     "cuisineTypeId, deliverySpeedId, offeredTimesId, "+
                     "tasteTypesId, ingredientTypesId, sauceTypesId, ingredientsDescription, rating, rating_count)"+
                     " values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15)",
-                    [request.body.restaurant_name, request.body.name,
-                      request.body.chineseMealName, request.body.category,
-                      request.body.price, request.body.picture_url,
-                      request.body.cuisineType, request.body.deliverySpeed,
-                      request.body.offeredTimes, request.body.tasteTypes,
-                      request.body.foodTypes, request.body.sauceTypes, request.body.ingredientsDescription,
-                      request.body.rating, 1
+                    [req.body.restaurant_name, req.body.name,
+                      req.body.chineseMealName, req.body.category,
+                      req.body.price, req.body.picture_url,
+                      req.body.cuisineType, req.body.deliverySpeed,
+                      req.body.offeredTimes, req.body.tasteTypes,
+                      req.body.foodTypes, req.body.sauceTypes, req.body.ingredientsDescription,
+                      req.body.rating, 0
                     ],
                     function(err, result) {
         if (err)
-         { console.error(err); response.send("Error " + err); }
+         { console.error(err); res.send("Error " + err); }
         else
-         { response.status(200).send({"status":"SUCCESS"}); }
+         { console.log("MEAL UPLOAD SUCCESS"); res.status(200).send({"status":"SUCCESS"}); }
       });
       done();
     });
 
   });
 
+  /*
+    updateMeal
+  */
+  app.post('/updateMeal', function (req, res) {
+    if (auth) {
+      if (req.body.password!=auth.lg1 || req.body.password!=auth.apc ||
+        req.body.password!=auth.grb || req.body.password!=auth.millano) {
+        console.log("ERROR: authentication failed");
+        res.send({status:"ERROR: authentication failed"});
+        return;
+      }
+    }
+    console.log("POST /updateMeal" + req.body.menuId);
+    pg.connect(DATABASE_URL, function(err, client, done) {
+      client.query("UPDATE meal SET ingredientsDescription = $2, "+
+                    "name = $3, chineseMealName = $4, price = $5 "+
+                    "WHERE meal.id = $1",
+                    [req.body.menuId, req.body.ingredientsDescription,
+                      req.body.name, req.body.chineseMealName, req.body.price
+                    ],
+                    function(err, result) {
+        if (err)
+        {
+          console.error(err); res.send("Error " + err);
+        }
+        else
+        {
+          console.log("menu update result:");
+          console.log(result.rows);
+          res.status(200).send({"status":"SUCCESS"});
+        }
+      });
+      done();
+    });
+  });
+
+  // DEPRECIATED
   app.post('/admin_only_update_menu', function (req, res) {
     console.log("/admin_only_update_menu menuID = " + req.body.menuId);
     pg.connect(DATABASE_URL, function(err, client, done) {
@@ -231,32 +289,17 @@ var init = function(app, DATABASE_URL) {
       done();
     });
   });
-
-  app.post('/admin_only_update_menu', function (req, res) {
-    console.log("/admin_only_update_menu menuID = " + req.body.menuId);
-    pg.connect(DATABASE_URL, function(err, client, done) {
-      client.query("UPDATE meal SET ingredientsDescription = $2, rating = $3, rating_count = $4"+
-                    "WHERE meal.id = $1",
-                    [req.body.menuId, req.body.ingredientsDescription, req.body.rating, 1],
-                    function(err, result) {
-        if (err)
-        {
-          console.error(err); res.send("Error " + err);
-        }
-        else
-        {
-          console.log("menu update result");
-          console.log(result.rows);
-          res.status(200).send({"status":"SUCCESS"});
-        }
-      });
-      done();
-    });
-  });
-
 
   //
   app.post('/deleteMeal', function (request, response) {
+    if (auth) {
+      if (req.body.password!=auth.lg1 || req.body.password!=auth.apc ||
+        req.body.password!=auth.grb || req.body.password!=auth.millano) {
+        console.log("ERROR: authentication failed");
+        res.send({status:"ERROR: authentication failed"});
+        return;
+      }
+    }
     //console.log("req.body = "+request.body);
     //console.log("restaurant id  = "+ request.body.restaurant_name);
     //console.log("offeredTimes = "+request.body.offeredTimes);
@@ -280,8 +323,6 @@ var init = function(app, DATABASE_URL) {
       done();
     });
   });
-
-
 
   app.get('/meal/all', function (request, response) {
     //console.log("database URL = "+(process.env.DATABASE_URL || LOCAL_DATABASE_URL));
